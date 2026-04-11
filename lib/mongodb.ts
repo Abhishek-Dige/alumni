cb83c7d459a1bcee1cfb89dc213236d4a925ca8b
Abhishek-Dige <abhishekdige22@gmail.com>
2026-04-12 00:33:16 +0530

    fix(auth): slight changes

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,7 +7,7 @@ let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   // prevent multiple connections in dev (hot reload)
-  let globalWithMongo = global as typeof globalThis & {
+  const globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
 
